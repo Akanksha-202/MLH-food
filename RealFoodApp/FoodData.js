fb002d02732c90d09cefcb28d84bbd90e6f9797e
Akanksha Jha <jhaakanksha202@gmail.com>
2023-03-05 05:52:59 +0530

    foodmenu

---
+++ RealFoodApp/FoodData.js
@@ -0,0 +1,64 @@
+export const FoodData = [
+    {
+        "Quisar Quiche": {
+          "img": "Quisar_Quiche.jpg",
+          "price": 9.99,
+          "name": "Quisar Quiche",
+          "rating": 4.5
+        },
+        "Martian Mountain Stew": {
+          "img": "Martian_Mountain_Stew.jpg",
+          "price": 12.99,
+          "name": "Martian Mountain Stew",
+          "rating": 4.2
+        },
+        "Cosmic Cavier": {
+          "img": "Cosmic_Cavier.jpg",
+          "price": 15.99,
+          "name": "Cosmic Cavier",
+          "rating": 4.7
+        },
+        "Interstellar S'mores": {
+          "img": "Interstellar_S'mores.jpg",
+          "price": 7.99,
+          "name": "Interstellar S'mores",
+          "rating": 4.0
+        },
+        "Gorgomaxian Meklent": {
+          "img": "Gorgomaxian_Meklent.jpg",
+          "price": 11.99,
+          "name": "Gorgomaxian Meklent",
+          "rating": 4.3
+        },
+        "Red Planet Antipasto": {
+          "img": "Red_Planet_Antipasto,jpg",
+          "price": 8.99,
+          "name": "Red Planet Antipasto",
+          "rating": 4.6
+        },
+        "Thaloxian Kralix": {
+          "img": "Thaloxian_Kralix.jpg",
+          "price": 10.99,
+          "name": "Thaloxian Kralix",
+          "rating": 4.1
+        },
+        "Arlorxian Daxoblade": {
+          "img": "Arlorxian_Daxoblade",
+          "price": 13.99,
+          "name": "Arlorxian Daxoblade",
+          "rating": 4.4
+        },
+        "Cosmic Crab Cakes": {
+          "img": "Cosmic_Crab_Cakes.jpg",
+          "price": 10.99,
+          "name": "Cosmic Crab Cakes",
+          "rating": 4.6
+        },
+        "Intergalactic Goulash": {
+          "img": "Intergalactic_Goulash.jpg",
+          "price": 8.99,
+          "name": "Intergalactic Goulash",
+          "rating": 4.4
+        }
+    }
+  ]
