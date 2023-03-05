f48222fce246bfec045d3eadd3d65d6f4b3dc820
Riaaaa03 <riasindwani@gmail.com>
2023-03-05 06:04:01 +0530

    ui updated

--- RealFoodApp/components/navbar.js
+++ RealFoodApp/components/navbar.js
@@ -1,13 +1,13 @@
 const navbar = () => {
     return `        <div class="nav">
-    <div><a href="./index.html"><img src="./logo2.png" alt="food app"></a></div>
+    <div><a href="./userdash.html"><img src="./logo2.png" alt="food app" width="100" height="50"></a></div>
 
     <ul>
        
         <li><a href="wallet.html">Wallet</a></li>
         <li><a href="like.html">Favorite</a></li>
-        <li><a href="cart.html">Cart</a><sup></sup></li>
-        
+        <li><a href="cart.html">Cart</a></li>
+        <li><a href="feedback.html">Feedback</a></li>
         <li><a href="signup.html">SignOut</a></li>
     </ul>
 </div>`
